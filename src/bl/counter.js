3184151cf919cbe43a32d10315dea2a7fa1d8476
Aleksey Manchinskiy <aleman@list.ru>
2022-10-15 15:55:01 +0300

    ver 2022-10-15 (2)

--- src/bl/counter.js
+++ src/bl/counter.js
@@ -4,10 +4,12 @@ const request = require('sync-request');
 module.exports.GetCounter = (url) =>
 {
     // Получаем данные
+    console.log('GetCounter url = ', url);
+
     const res = request('GET', url);
     const data = JSON.parse(res.getBody('utf8'));
     
-    console.log('GET', data.result);
+    console.log('GetCounter result = ', data.result);
 
     return data.result;
 }
@@ -15,8 +17,12 @@ module.exports.GetCounter = (url) =>
 module.exports.SetCounter = (url) =>
 {
     // Сохраняем данные
+    console.log('SetCounter url = ', url);
+
     const res = request('POST', url);
     const data = JSON.parse(res.getBody('utf8'));
 
-    console.log('POST', data.result);
+    console.log('SetCounter result = ', data.result);
+
+    return data.result;
 }
